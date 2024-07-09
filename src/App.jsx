267b927dbfbc2ffa15b6b79ad4015d27d18c351c
apl1978 <apl_1978@yahoo.com>
2024-07-09 17:19:37 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Stars } from './components/Stars'
+import './App.css'
+
+const count = 4;
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Stars count={count} />
+    </div>
+  )
+}
+
+export default App
